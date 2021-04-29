68c271ab60f5134364ea108726f7f3a6f8f6071a
Ruslan Baikulov <ruslan1123@gmail.com>
2021-04-29 21:49:46 +0300

    test: Add DeltaTimeProfiler test

---
+++ tests/test_timing.py
@@ -0,0 +1,20 @@
+import time
+import pytest
+
+from rosny.timing import DeltaTimeProfiler
+
+
+@pytest.mark.parametrize("start", [True, False])
+def test_delta_time_profiler(start):
+    profiler = DeltaTimeProfiler()
+
+    if start:
+        time.sleep(0.1)
+    for _ in range(100):
+        if start:
+            profiler.start()
+        time.sleep(0.01)
+        profiler.end()
+    assert pytest.approx(profiler.mean, rel=6e-2) == 0.01
+    profiler.reset()
+    assert profiler.count == 0
