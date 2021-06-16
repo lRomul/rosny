2bb1a273d425669066fac0bc5b927d33bb1a1260
Ruslan Baikulov <ruslan1123@gmail.com>
2021-06-16 13:55:11 +0300

    feat: Add MeanMeter and EMAMeter

--- rosny/utils.py
+++ rosny/utils.py
@@ -18,3 +18,36 @@ def setup_logger(name: str) -> logging.Logger:
         logger.handlers.clear()
     logger.addHandler(stdout)
     return logger
+
+
+class MeanMeter:
+    def __init__(self):
+        self.mean: float = 0.0
+        self.count: int = 0
+
+    def reset(self):
+        self.mean = 0.0
+        self.count = 0
+
+    def update(self, value: float, n: int = 1):
+        self.count += n
+        self.mean += (value - self.mean) / self.count
+
+
+class EMAMeter:
+    def __init__(self, alpha: float):
+        self.alpha = alpha
+        self.one_minus_alpha = 1 - alpha
+        self.mean: float = 0.0
+        self.count = 0
+
+    def reset(self):
+        self.mean = 0.0
+        self.count = 0
+
+    def update(self, value: float):
+        if self.count:
+            self.mean = self.alpha * value + self.one_minus_alpha * self.mean
+        else:
+            self.mean = value
+        self.count += 1
