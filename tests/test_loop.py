3af41563d7f1aca4b6c1dec39b6c3dc4f7bf4543
Ruslan Baikulov <ruslan1123@gmail.com>
2021-06-22 22:08:58 +0300

    test: Merge process and thread tests to loop stream test

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -3,51 +3,60 @@ import time
 import signal
 import pytest
 from typing import Optional
-from multiprocessing import Process, Value
+from multiprocessing import Value
 
-from rosny.process import ProcessStream
+from rosny import ThreadStream, ProcessStream
 from rosny.signal import SignalException
 
 
-class CustomStream(ProcessStream):
-    def __init__(self,
-                 loop_rate: Optional[float] = None,
-                 min_sleep: float = 1e-9,
-                 init_param=None,
-                 another_param=43):
-        super().__init__(loop_rate=loop_rate, min_sleep=min_sleep)
-        self.init_param = init_param
-        self.another_param = another_param
-        self.count = Value('i', 0)
+@pytest.fixture(scope='module', params=[ThreadStream, ProcessStream])
+def loop_stream_class(request):
+    return request.param
 
-    def work(self):
-        self.count.value += 1
+
+@pytest.fixture(scope='module')
+def custom_stream_class(loop_stream_class):
+    class CustomStream(loop_stream_class):
+        def __init__(self,
+                     loop_rate: Optional[float] = None,
+                     min_sleep: float = 1e-9,
+                     init_param=None,
+                     another_param=43):
+            super().__init__(loop_rate=loop_rate, min_sleep=min_sleep)
+            self.init_param = init_param
+            self.another_param = another_param
+            self.count = Value('i', 0)
+
+        def work(self):
+            self.count.value += 1
+
+    return CustomStream
 
 
 @pytest.fixture(scope='function')
-def stream():
-    stream = CustomStream()
+def stream(custom_stream_class):
+    stream = custom_stream_class()
     yield stream
     stream.stop()
     stream.join()
 
 
 class TestProcessStream:
-    def test_init(self):
-        stream = CustomStream(loop_rate=30.0, min_sleep=0.001)
+    def test_init(self, custom_stream_class):
+        stream = custom_stream_class(loop_rate=30.0, min_sleep=0.001)
         assert stream.init_param is None
         assert stream.another_param == 43
         stream.compile()
         assert stream.rate_manager.loop_rate == 30.0
         assert stream.rate_manager.min_sleep == 0.001
 
-        stream = CustomStream(init_param=42)
+        stream = custom_stream_class(init_param=42)
         assert stream.init_param == 42
         assert stream.another_param == 43
         assert stream.rate_manager.loop_rate is None
         assert stream.rate_manager.min_sleep == 1e-9
 
-    def test_compile(self, stream: CustomStream):
+    def test_compile(self, stream):
         assert not stream.compiled()
         assert not stream._compiled
         assert stream.name.startswith('CustomStream-')
@@ -58,7 +67,7 @@ class TestProcessStream:
         assert stream.name == 'CustomStream'
         assert stream.logger.name == 'CustomStream'
 
-    def test_start(self, stream: CustomStream):
+    def test_start(self, stream):
         assert not stream.compiled()
         assert stream.stopped()
         assert stream.joined()
@@ -66,43 +75,41 @@ class TestProcessStream:
         assert stream.compiled()
         assert not stream.stopped()
         assert not stream.joined()
-        assert isinstance(stream._driver, Process)
         assert stream._driver.is_alive()
 
-    def test_wait(self, stream: CustomStream, time_meter):
+    def test_wait(self, stream, time_meter):
         time_meter.start()
         stream.start()
         stream.wait(timeout=0.1)
         time_meter.end()
         assert pytest.approx(time_meter.mean, abs=0.05) == 0.1
 
-    def test_stop(self, stream: CustomStream):
+    def test_stop(self, stream):
         stream.start()
         stream.wait(timeout=0.01)
         assert not stream.stopped()
         stream.stop()
         assert stream.stopped()
 
-    def test_join(self, stream: CustomStream):
+    def test_join(self, stream):
         stream.start()
         stream.wait(timeout=0.01)
         stream.stop()
         assert stream.stopped()
         assert not stream.joined()
-        assert isinstance(stream._driver, Process)
         stream.join()
         assert stream.stopped()
         assert stream.joined()
         assert stream._driver is None
 
-    def test_loop_rate_work(self, stream: CustomStream):
+    def test_loop_rate_work(self, stream):
         stream.rate_manager.loop_rate = 120
         stream.start()
         stream.wait(timeout=3.0)
         assert pytest.approx(stream.count.value, rel=0.1) == 360
 
-    def test_join_timeout(self, time_meter):
-        class SleepStream(ProcessStream):
+    def test_join_timeout(self, loop_stream_class, time_meter):
+        class SleepStream(loop_stream_class):
             def work(self):
                 time.sleep(3)
 
@@ -119,24 +126,23 @@ class TestProcessStream:
         stream.join()
         assert stream.joined()
 
-    def test_min_sleep(self, stream: CustomStream):
+    def test_min_sleep(self, stream):
         stream.rate_manager.min_sleep = 0.01
         stream.start()
         stream.wait(timeout=0.1)
         assert stream.count.value <= 11
 
-    def test_double_start(self, stream: CustomStream):
+    def test_double_start(self, stream):
         assert stream._driver is None
         stream.start()
-        assert isinstance(stream._driver, Process)
-        process = stream._driver
+        driver = stream._driver
         stream.start()
-        assert stream._driver is process
+        assert stream._driver is driver
         stream.stop()
         stream.join()
         assert stream._driver is None
 
-    def test_restart(self, stream: CustomStream):
+    def test_restart(self, stream):
         stream.start()
         stream.wait(timeout=1)
         assert not stream.stopped()
@@ -154,25 +160,25 @@ class TestProcessStream:
         assert stream.stopped()
         assert stream.count.value > 30
 
-    def test_wrong_restart(self, stream: CustomStream):
+    def test_wrong_restart(self, stream):
         stream.start()
-        process = stream._driver
+        driver = stream._driver
         stream.wait(timeout=1)
         assert not stream.stopped()
         stream.stop()
-        assert stream._driver is process
+        assert stream._driver is driver
         assert stream.stopped()
 
         stream.start()
-        assert stream._driver is process
+        assert stream._driver is driver
         assert stream.stopped()
 
         stream.join()
         assert stream._driver is None
         assert stream.joined()
 
-    def test_exception_catching(self):
-        class ExceptionStream(ProcessStream):
+    def test_exception_catching(self, loop_stream_class):
+        class ExceptionStream(loop_stream_class):
             def __init__(self):
                 super().__init__()
                 self.exception = Value('i', 0)
@@ -192,7 +198,7 @@ class TestProcessStream:
         stream.stop()
         stream.join()
 
-    def test_handle_signal(self, stream: CustomStream):
+    def test_handle_signal(self, stream):
         stream.start()
         time.sleep(0.1)
         with pytest.raises(SignalException):
@@ -204,8 +210,8 @@ class TestProcessStream:
         assert stream.joined()
 
     @pytest.mark.parametrize("daemon", [True, False])
-    def test_daemon(self, daemon):
-        class DaemonStream(ProcessStream):
+    def test_daemon(self, loop_stream_class, daemon):
+        class DaemonStream(loop_stream_class):
             def __init__(self, daemon):
                 super().__init__(daemon=daemon)
 
