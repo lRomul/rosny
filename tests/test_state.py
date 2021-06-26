3542e7a5a0514cddea61a65ce4b8acf493e49d9e
Ruslan Baikulov <ruslan1123@gmail.com>
2021-06-27 01:04:44 +0300

    test: Test removing _manager for pickle

--- tests/test_state.py
+++ tests/test_state.py
@@ -3,28 +3,33 @@ from rosny.state import CommonState
 
 
 @pytest.fixture(scope='function')
-def internal_state() -> CommonState:
+def common_state() -> CommonState:
     return CommonState()
 
 
-class TestInternalState:
-    def test_set_clear_exit_event(self, internal_state):
-        assert not internal_state._exit_event.is_set()
-        assert not internal_state.exit_is_set()
-        internal_state.set_exit()
-        assert internal_state._exit_event.is_set()
-        assert internal_state.exit_is_set()
-        internal_state.clear_exit()
-        assert not internal_state._exit_event.is_set()
-        assert not internal_state.exit_is_set()
+class TestCommonState:
+    def test_set_clear_exit_event(self, common_state):
+        assert not common_state._exit_event.is_set()
+        assert not common_state.exit_is_set()
+        common_state.set_exit()
+        assert common_state._exit_event.is_set()
+        assert common_state.exit_is_set()
+        common_state.clear_exit()
+        assert not common_state._exit_event.is_set()
+        assert not common_state.exit_is_set()
 
-    def test_state_wait_exit(self, internal_state, time_meter):
-        assert not internal_state.exit_is_set()
+    def test_state_wait_exit(self, common_state, time_meter):
+        assert not common_state.exit_is_set()
 
         time_meter.start()
-        internal_state.wait_exit(timeout=0.1)
+        common_state.wait_exit(timeout=0.1)
         time_meter.end()
         assert pytest.approx(time_meter.mean, rel=0.05) == 0.1
 
-        internal_state.set_exit()
-        internal_state.wait_exit()
+        common_state.set_exit()
+        common_state.wait_exit()
+
+    def test_get_set_state(self, common_state):
+        state = common_state.__getstate__()
+        common_state.__setstate__(state)
+        assert common_state._manager is None
